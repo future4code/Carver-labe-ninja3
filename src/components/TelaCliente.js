8d90ca7274d5b087f3ebc16af727623f38c9dafe
Leticia Silva <wakawashi2030@gmail.com>
2021-11-24 21:55:43 -0300

    Criado componente TelaCliente(filtro)
    Algumas chamadas estão no app, mas não constam aqui.

---
+++ src/components/TelaCliente.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class extends React.Component{
+  render(){
+      
+    return <div>
+        
+        Valor Minimo:
+        <input type={"number"}
+        value={this.props.minimo}
+        onChange = {this.props.onChangeMinimo}
+        />
+
+        Valor Maximo:
+        <input type={"number"}
+        value={this.props.maximo}
+        onChange = {this.props.onChangeMaximo}
+        
+        />
+
+        Busca por titulo:
+        <input type={"text"}
+        value={this.props.buscaPorTitulo}
+        onChange = {this.props.onChangeBuscaPorTitulo}
+        
+        />
+
+    </div>
+  }
+
+
+}
+

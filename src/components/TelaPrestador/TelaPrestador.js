de2cb2974aa26ec7ac0d6953c522486f3213971a
Luiza Cioffi <luizagac@gmail.com>
2021-11-25 11:38:25 -0300

    Correção do componente TelaPrestador, adequação do componente App e componente de teste deletado

--- src/components/TelaPrestador/TelaPrestador.js
+++ src/components/TelaPrestador/TelaPrestador.js
@@ -37,15 +37,15 @@ export default class TelaPrestador extends React.Component {
     }
   
     // componentDidMount(){
-    
     // }
   
     postCreateJob = () => {
       const url = "https://labeninjas.herokuapp.com/jobs"
+      const precoNumero = parseFloat(this.state.preco);
       const body = {
         title: this.state.titulo,
         description: this.state.descricao,
-        price: this.state.preco,
+        price: precoNumero,
         paymentMethods: this.state.formasPagamento,
         dueDate: this.state.prazo,
       }
@@ -55,10 +55,10 @@ export default class TelaPrestador extends React.Component {
         }
       })
       .then((resp) => {
-        console.log(resp)
+        alert("Oba! Cadastro NINJA realizado com sucesso!")
       })
       .catch((erro) => {
-        console.log(erro.response)
+        alert("Ooops... Parece que algo deu errado, tente novamente mais tarde.")
       })
     }
   
@@ -74,9 +74,13 @@ export default class TelaPrestador extends React.Component {
       this.setState({preco: event.target.value})
     }
   
-    handleFormasPagamento = (event) => {
-      this.setState({formasPagamento: event.target.value})
-    }
+    handleFormasPagamento = (event) => {    
+      const copiaFormasPagamento = [...event.target.value]    
+      this.setState({ formasPagamento: copiaFormasPagamento})
+    };
+    // handleFormasPagamento = (event) => {
+    //   this.setState({formasPagamento: event.target.value})
+    // }
   
     handlePrazo = (event) => {
       this.setState({prazo: event.target.value})
@@ -87,50 +91,51 @@ export default class TelaPrestador extends React.Component {
               <div>
                   <p>Prestador</p>
                   <h1>Cadastre o seu serviço aqui!</h1>
-                  <FormularioInput 
-                  placeholder={"Título"}
-                  value={this.state.titulo}
-                  onChange={this.handleTitulo}
-                  />
+                  <form>
+                    <FormularioInput 
+                    placeholder={"Título"}
+                    value={this.state.titulo}
+                    onChange={this.handleTitulo}
+                    />
 
-                  <FormularioInput 
-                  placeholder={"Descrição"}
-                  value={this.state.descricao}
-                  onChange={this.handleDescricao}
-                  />
+                    <FormularioInput 
+                    placeholder={"Descrição"}
+                    value={this.state.descricao}
+                    onChange={this.handleDescricao}
+                    />
 
-                  <FormularioInput 
-                  placeholder={"Preço"}
-                  value={this.state.preco}
-                  onChange={this.handlePreco}
-                  />
-                
-                  {/* Quero ver como mandar esse dado para a API*/}                  
+                    <FormularioInput 
+                    placeholder={"Preço"}
+                    value={this.state.preco}
+                    onChange={this.handlePreco}
+                    />
                   
-                    <p>Qual a forma de pagamento aceita?</p>
-                    <select multiple value={this.state.formasPagamento} onChange={this.handleFormasPagamento}>
-                      <option value="cartdebito">Cartão de Débito</option>
-                      <option value="cartcredito">Cartão de Crédito</option>
-                      <option value="pix">Pix</option>
-                      <option value="boleto">Boleto</option>
-                      <option value="dinheiro">Dinheiro</option>
-                    </select>
-                    {/* <button type="submit">Adicionar</button> */}
+                    {/* Quero ver como mandar esse dado para a API*/}                  
+                      <p>Qual a forma de pagamento aceita?</p>
+                      <select value={this.state.formasPagamento} onChange={this.handleFormasPagamento}>
+                        <option value="cartdebito">Cartão de Débito</option>
+                        <option value="cartcredito">Cartão de Crédito</option>
+                        <option value="pix">Pix</option>
+                        <option value="boleto">Boleto</option>
+                        <option value="dinheiro">Dinheiro</option>
+                      </select>
+                      
+                    {/* <FormularioInput 
+                    placeholder={"Formas de pagamento"}
+                    value={this.state.formasPagamento}
+                    onChange={this.handleFormasPagamento}
+                    /> */}
+
+                    <FormularioInput 
+                    placeholder={"Prazo"}
+                    value={this.state.prazo}
+                    onChange={this.handlePrazo}
+                    />
+
+                    <button type="submit" onClick={this.postCreateJob}>Cadastrar seu trabalho Ninja</button>
+                  </form>
+
                   
-
-                  {/* <FormularioInput 
-                  placeholder={"Formas de pagamento"}
-                  value={this.state.formasPagamento}
-                  onChange={this.handleFormasPagamento}
-                  /> */}
-
-                  <FormularioInput 
-                  placeholder={"Prazo"}
-                  value={this.state.prazo}
-                  onChange={this.handlePrazo}
-                  />
-
-                  <button onClick={this.postCreateJob}>Cadastrar seu trabalho Ninja</button>
               </div>
           )
       }

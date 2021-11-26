26f9b5c95f88efed30562a27116f57a459d8efda
Leticia Silva <wakawashi2030@gmail.com>
2021-11-25 22:32:55 -0300

    criado tela do cliente, filtro e ordenação (comandos iniciais)

--- src/components/TelaCliente/TelaCliente.js
+++ src/components/TelaCliente/TelaCliente.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import axios from 'axios';
+// import Filtro from "..Filtro/Filtro";
 
 export default class extends React.Component{
   render(){
-
+      
     return <div>
-
+        
         Valor Minimo:
         <input type={"number"}
         value={this.props.minimo}
@@ -15,14 +17,14 @@ export default class extends React.Component{
         <input type={"number"}
         value={this.props.maximo}
         onChange = {this.props.onChangeMaximo}
-
+        
         />
 
         Busca por titulo:
         <input type={"text"}
         value={this.props.buscaPorTitulo}
         onChange = {this.props.onChangeBuscaPorTitulo}
-
+        
         />
 
     </div>
